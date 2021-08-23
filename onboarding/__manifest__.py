a49ff947eac4d5a64c52a76e1d0d5d8dfe8f9361
Esteban <88267322+CLUBCBCDEV@users.noreply.github.com>
2021-08-23 21:12:32 +0200

    Update __manifest__.py

--- onboarding/__manifest__.py
+++ onboarding/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "onboarding",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        onboarding CBC members""",
 
     'description': """
         Long description of module's purpose
